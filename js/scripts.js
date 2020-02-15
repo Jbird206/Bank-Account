a3f2047912a1edf6ecf8cdba19646702af700095
Jason M <jbird206@gmail.com>
2020-02-15 14:48:17 -0800

    Add files via upload
    more Html, deposit function, printing name to account details, adds deposit to the initial deposit sum.

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,35 @@
 // Business Logic for AddressBook ---------
 function centralBank() {
-    this.accounts = [],
-    this.currentId = 0
-  }
-  
-  centralBank.prototype.addAccount = function (account){
-    account.id = this.assignId();
-    this.accounts.push(account);
-  }
+  this.accounts = [],
+  this.currentId = 0
+}
 
-  centralBank.prototype.assignId = function() {
-    this.currentId += 1;
-    return this.currentId;
-  }
+centralBank.prototype.addAccount = function (account){
+  account.id = this.assignId();
+  this.accounts.push(account);
+}
 
-   // Business Logic for Accounts ---------
-   function Account(nameAccount, checkingAccount) {
-    this.nameAccount = nameAccount,
-    this.checkingAccount = checkingAccount
-  }
+centralBank.prototype.assignId = function() {
+  this.currentId += 1;
+  return this.currentId;
+}
+
+ // Business Logic for Accounts ---------
+var Account= function (nameAccount, checkingAccount) {
+  this.nameAccount = nameAccount,
+  this.checkingAccount = checkingAccount;
+  return this.nameAccount + this.checkingAccount;
+};
+
+var add  = function(inputtedcheckingAccount, accountDeposit) {
+  return inputtedcheckingAccount + accountDeposit;
+}
+
+var subtract = function(inputtedcheckingAccount, accountWithdraw) {
+  return inputtedcheckingAccount - accountWithdraw;
+}
+
+
 
 // User Interface Logic ---------
 var centralBank = new centralBank();
@@ -27,17 +38,36 @@ var centralBank = new centralBank();
 
 
 $(document).ready(function() {
-    //attachAccountListeners();
-  $("form#new-account").submit(function(event) {
-    event.preventDefault();
-    var inputtednameAccount = $("input#new-name-account").val();
-    var inputtedcheckingAccount = parseInt($("input#new-checking-account").val());
-    var newAccount = new Account(inputtednameAccount, inputtedcheckingAccount);
-    centralBank.addAccount(newAccount);
-    console.log(centralBank.accounts);
-    $("#output").text("$" + newAccount.checkingAccount);
-    //displayAccountDetails(addressBook);
-  });
-
+  //attachAccountListeners();
+$("form#new-account").submit(function(event) {
+  event.preventDefault();
+  var inputtednameAccount = $("input#new-name-account").val();
+  var inputtedcheckingAccount = parseInt($("input#new-checking-account").val());
+  var newAccount = new Account(inputtednameAccount, inputtedcheckingAccount);
+  centralBank.addAccount(newAccount);
+  console.log(centralBank.accounts);
+  $("#outputName").html(inputtednameAccount);
+  $("#output").html( newAccount.checkingAccount);
+  
 });
 
+
+
+
+$("form#acctManagement").submit(function(event) {
+  event.preventDefault();
+var accountDeposit = parseInt($("input#accountDeposit").val());
+var inputtedcheckingAccount = parseInt($("input#new-checking-account").val());
+var result = add(accountDeposit, inputtedcheckingAccount);
+//$("#output2").html(result);
+$("#output").html(result);
+});
+
+// $("form#acctManagement").submit(function(event) {
+//   event.preventDefault();
+//   var accountWithdraw = parseInt($("input#accountWithdraw").val());
+//   var inputtedcheckingAccount = parseInt($("input#new-checking-account").val());
+//   var result2 = subtract(accountWithdraw, inputtedcheckingAccount);
+//   $("#output2").text(result2);
+// });
+});
